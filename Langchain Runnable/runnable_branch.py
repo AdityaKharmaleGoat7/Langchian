f5462b495325cf45d2279e77d36aa27f11c688be
AdityaKharmaleGoat7 <adityakharmale7@gmail.com>
2025-08-11 22:22:46 +0530

    Runnable

---
+++ Langchain Runnable/runnable_branch.py
@@ -0,0 +1,3 @@
+# In runnable branch, you can create a sequence of runnables
+# that can be executed in order. This is useful for creating
+# complex workflows that require multiple steps. Like if/else,comditions, etc.
