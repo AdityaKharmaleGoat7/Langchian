54fb59768229e55dda26e49704763fd41c0dcfe2
AdityaKharmaleGoat7 <adityakharmale7@gmail.com>
2025-08-12 12:29:57 +0530

    Document loader

---
+++ Langchain Document Loader/text_loader.py
@@ -0,0 +1,26 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+load_dotenv()
+
+model = ChatOpenAI()
+
+prompt = PromptTemplate(
+    template="Extract the summary from the following text: {text}",
+    input_variables=["text"],
+)
+
+pareser = StrOutputParser()
+
+loader = TextLoader("cricket.txt", encoding="utf-8")
+
+docs = loader.load()
+
+print(docs[0].page_content)
+print(docs[0].metadata)
+
+chain = model | prompt | pareser
+
+print(chain.invoke({"text": docs[0].page_content}))
